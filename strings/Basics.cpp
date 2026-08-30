44f2acf3fa5d30d6270f0a17f3bb5c1ae119269b
Sumit Ganesh Ade <sumitade324@gmail.com>
2026-08-30 17:28:53 +0530

    Delete strings directory

--- strings/Basics.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-#include<string>
-using namespace std;
-int main(){
-    string x = "Sumit Ade is a Goat";
-    cout<<x<<endl;
-}
-
-
--- strings/add_in_string.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include<string>
-using namespace std;
-int main(){
-    int x,y;
-    cin>>x>>y;
-    cout<<x+y;
-}
